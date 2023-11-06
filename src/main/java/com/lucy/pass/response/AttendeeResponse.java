0a6c29c0546dca94ca5b025805da86642c8d037a
haebin.lee <luckylucy0717@gmail.com>
2023-11-05 18:11:06 -0800

    add attendStatus

--- src/main/java/com/lucy/pass/response/AttendeeResponse.java
+++ src/main/java/com/lucy/pass/response/AttendeeResponse.java
@@ -1,5 +1,6 @@
 package com.lucy.pass.response;
 
+import com.lucy.pass.dto.AttendanceStatus;
 import com.lucy.pass.repository.Attendee;
 import lombok.Getter;
 
@@ -10,11 +11,13 @@ public class AttendeeResponse {
     private final String name;
     private final String email;
     private final String phone;
+    private final AttendanceStatus status;
 
     public AttendeeResponse(Attendee attendee) {
         this.id = attendee.getId();
         this.name = attendee.getName();
         this.email = attendee.getEmail();
         this.phone = attendee.getPhone();
+        this.status = attendee.getStatus();
     }
 }
